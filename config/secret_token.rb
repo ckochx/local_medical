b14e62dd2c111e83bf5cd0f390b92506b582100a
Christian Koch <ckochx@gmail.com>
2015-07-21 21:19:09 -0500

    goddamnit

--- config/secret_token.rb
+++ config/secret_token.rb
@@ -10,3 +10,4 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 LocalApp::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+LocalApp::Application.config.secret_token = ENV['SECRETTOKEN']
